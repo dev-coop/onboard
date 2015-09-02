fed391514d326460b0e6243ad95848b99e693485
Eric Carmichael <eric@ckcollab.com>
2015-09-02 01:59:24 -0700

    adds github api and slack api integration, i believe both are working but very untested

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # onboard
 a slack integration for adding team members to github, slack, and screenhero. hosted on heroku
+
+
+# Usage
+
+### Setup env vars
+
+```bash
+export PORT=5000
+
+export GITHUB_ORGANIZATION_NAME=the_gang
+export GITHUB_API_KEY=1234567890
+
+export SLACK_API_TOKEN=1234567890  # the key you'd use to talk to Slack API
+export SLACK_API_SECRET=1234567890  # the key Slack sends to us to verify it's really Slack
+export SLACK_TEAM_NAME=paddys_pub
+```
+
+### Run
+
+`python server.py` 
+
+It will start running on the port specified by the `PORT` env var or default `5000`
--- server.py
+++ server.py
@@ -1,10 +1,115 @@
+import json
 import os
-from bottle import route, run
+
+from bottle import get, run, post, request, HTTPError, response
+from requests import Session
+from requests import post as request_post
 
 
-@route("/")
-def hello_world():
-        return "Hello World!"
+# ----------------------------------------------------------------------------
+# Settings
+# ----------------------------------------------------------------------------
+env_var_names = (
+    'GITHUB_API_KEY',
+    'GITHUB_ORGANIZATION_NAME',
+    'SLACK_API_TOKEN',
+    'SLACK_API_SECRET',
+    'SLACK_TEAM_NAME',
+)
+env = {}
+for name in env_var_names:
+    env[name] = os.environ.get(name, None)
+    assert env[name], "Missing environment variable: %s" % name
 
 
+# ----------------------------------------------------------------------------
+# Helpers
+# ----------------------------------------------------------------------------
+def github_request(method, endpoint, data=None):
+    github_session = Session()
+    github_session.auth = (env['GITHUB_API_KEY'], 'x-oauth-basic')
+    base_url = 'https://api.github.com'
+    method_func = getattr(github_session, method.lower())
+    response = method_func(
+        base_url + endpoint,
+        data=data
+    )
+    return response
+
+
+def github_add_member_to_org(github_username):
+    return github_request(
+        'PUT',
+        '/orgs/%s/memberships/%s' % (env['GITHUB_ORGANIZATION_NAME'], github_username),
+        data=json.dumps({"role": "member"})
+    )
+
+
+def slack_invite(email):
+    print env['SLACK_API_TOKEN']
+    return request_post(
+        'https://%s.slack.com/api/users.admin.invite' % (env['SLACK_TEAM_NAME']),
+        data=json.dumps({
+            "token": env['SLACK_API_TOKEN'],
+            "email": email,
+            "set_active": True,
+        }),
+        headers={
+            'Content-type': 'application/json',
+            'Accept': 'text/plain'
+        }
+    )
+
+
+
+
+import ipdb;ipdb.set_trace()
+
+
+
+
+
+
+# ----------------------------------------------------------------------------
+# Views / server
+# ----------------------------------------------------------------------------
+@post('/add')
+def add():
+    # Parse input
+    if request.forms.get('token') != env['SLACK_API_TOKEN']:
+        # Make sure we got a request from the actual slack server not some ass hole
+        return HTTPError(status=403)
+
+    text = request.forms.get('text')
+    if not text or len(text.split(' ')) != 2:
+         response.status_code = 400
+         return {"error": "Invalid text input, should look like /onboard <github name>; <email>"}
+
+    github_username, email = text.split(' ')
+    github_username = github_username.strip()
+    email = email.strip()
+
+    # Add to github
+    resp = github_add_member_to_org(github_username)
+    if resp.status_code != 200:
+        response.status_code = 500
+        return {"error": "Bad response from Github (%s): %s" % (resp.status_code, resp.content)}
+
+    # Add to slack
+    resp = slack_invite(email)
+    if resp.status_code != 200:
+        response.status_code = 500
+        return {"error": "Bad response from Slack (%s): %s" % (resp.status_code, resp.content)}
+
+    # Add to screenhero
+    # TODO
+    return "Successfully added user to Github, Slack and Screenhero... wee!"
+
+
+@get("/")
+def nice_index():
+    return "Hello, I am an <a href='https://github.com/dev-coop/onboard'>onboarding bot</a>!"
+
+
+# Heroku sets PORT env var
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
